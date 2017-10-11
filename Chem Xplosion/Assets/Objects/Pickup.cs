54108fbd7a2cbcdbcb1836bdb2e1c40ee74d4d64
apancha <apanchatcharam@ggc.edu>
2017-10-11 16:33:56 -0400

    demo-1 mostly complete (added rooms chemicals and all the scripts.

---
+++ Chem Xplosion/Assets/Objects/Pickup.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	Transform hands;
+	public GameObject loc;
+	bool isGrabbed = false;
+	GameObject player;
+	PlayerController playerController;
+	private RaycastHit hit;
+	private GameObject hold;
+
+	// Use this for initialization
+	void Start () {
+		hands = GameObject.Find ("PickupLoc").transform;
+		player = GameObject.Find ("Player");
+		playerController = player.GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1") && isGrabbed == false) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 2.5f)) {
+				if (hit.collider.gameObject.tag == "Pickupable"){
+					this.isGrabbed = true;
+					Grab (hit.collider.gameObject);
+					GetComponent<Rigidbody> ().isKinematic = true;
+				}
+			}
+
+
+		} else if (Input.GetButtonDown("Fire1") && isGrabbed == true) {
+			isGrabbed = false;
+			GetComponent<Rigidbody> ().useGravity = true;
+			GetComponent<Rigidbody> ().isKinematic = false;
+			this.transform.parent = null;
+			playerController.isLooking = false;
+		} 
+
+		if (isGrabbed == true) {
+			playerController.isLooking = true;
+			float v = Input.GetAxisRaw ("Vertical");
+			float h = Input.GetAxisRaw ("Horizontal");
+			hold.transform.Rotate (v, 0, h);
+		}
+
+	}
+
+	void Grab (GameObject o) {
+		if (isGrabbed) {
+			hold = o;
+			o.transform.parent = loc.transform;
+		o.GetComponent<Rigidbody> ().useGravity = false;
+		o.transform.position = hands.position;
+		}
+	}
+
+	void OnMouseUp () {
+		GetComponent<Rigidbody> ().useGravity = true;
+	}
+}
---
+++ Chem Xplosion/Assets/Onhit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onhit : MonoBehaviour {
+	public GameObject door;
+	public int counter =0;
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (counter == 3) {
+			destroyDoor ();
+		}
+	}
+		void OnCollisionEnter(Collision col)
+		{
+			if (col.gameObject.tag == "Pickupable")
+			{
+			counter++;
+			}
+		}
+	void destroyDoor(){
+		Destroy (door);
+		Instantiate (explosion,transform.position, Quaternion.identity);
+		Invoke ("destroyExplosion", 3);
+	}
+	void destroyExplosion(){
+		Destroy (explosion);
+	}
+	}
+
