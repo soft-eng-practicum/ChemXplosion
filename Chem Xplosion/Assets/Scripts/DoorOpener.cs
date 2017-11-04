cb26cef7f030216277d0c17f1876d7bd252fd848
apancha <apanchatcharam@ggc.edu>
2017-11-03 20:43:55 -0400

    small bugfixes

--- Chem Xplosion/Assets/Scripts/DoorOpener.cs
+++ Chem Xplosion/Assets/Scripts/DoorOpener.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DoorOpener : MonoBehaviour {
 
 	private Animator dAnimate;
+	public bool close = true;
 	// Use this for initialization
 	void Start () {
 		dAnimate = GetComponent<Animator> ();
@@ -13,11 +14,19 @@ public class DoorOpener : MonoBehaviour {
 	void OnTriggerEnter(Collider col){
 		if (col.tag == "Player") {
 			dAnimate.SetBool ("open", true);
+			close = false;
 		}
 		}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (close == false) {
+			Invoke ("closeDoor", 3);
+		}
+	}
+
+	void closeDoor(){
+		dAnimate.SetBool ("open", false);
+		close = true;
 	}
 }
