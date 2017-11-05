93bf5df2be8387a39e4e2b6b2225fc1a4c299079
apancha <apanchatcharam@ggc.edu>
2017-11-05 12:24:21 -0500

    added props in room

---
+++ Chem Xplosion/Assets/LightChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChange : MonoBehaviour {
+	public Light lt;
+	public static int puzzlecount;
+	// Use this for initialization
+	void Start () {
+		lt = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		puzzlecount = Onhit.counter;
+
+		if (puzzlecount == 2) {
+			lt.color = Color.green;
+		}
+	}
+}
