a32081cec28c1377ccfe69aa18fbd2f87d1da41f
Rae <Rae.vroman@gmail.com>
2018-03-07 18:33:43 -0500

    added assets

---
+++ Chem Xplosion/Assets/Scripts/Puzzle2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2 : MonoBehaviour {
+
+    public static bool isComplete_Puzzle_2 = false;
+    public static int puzzleTwoCounter = 0;
+    public GameObject[] puzzleTwoItems;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Accept")
+        {
+            puzzleTwoCounter++;
+        }
+        if (col.gameObject.tag == "Deny")
+        {
+            puzzleTwoCounter--;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Accept")
+        {
+            puzzleTwoCounter--;
+        }
+        if (col.gameObject.tag == "Deny")
+        {
+            puzzleTwoCounter++;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puzzleTwoCounter == 1)
+        {
+            isComplete_Puzzle_2 = true;
+            foreach (GameObject item in puzzleTwoItems)
+            {
+                Destroy(item);
+            }
+        }
+    }
+}
