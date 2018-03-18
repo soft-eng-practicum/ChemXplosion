f4c3b5e468e13b9f2f4d200463fd7224e8c7cb5d
Rae <Rae.vroman@gmail.com>
2018-03-18 16:20:27 -0400

    finished puzzle 2, you can now pour a chemical and it will solve the puzzle if it's the right chemical. If it's the wrong chemical, something explodes and the door stays shut

---
+++ Chem Xplosion/Assets/Scripts/Pour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pour : MonoBehaviour
+{
+
+    private Animator animate;
+    public bool poured;
+    public Pickup pickup;
+    public bool isPoured; 
+
+    // Use this for initialization
+    void Start()
+    {
+        poured = false;
+        isPoured = false;
+        animate = GetComponent<Animator>();
+        pickup = GetComponent<Pickup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("e") && pickup.isGrabbed == true)
+        {
+            animate.SetBool("poured", true);
+            isPoured = true;
+        }
+        if (Input.GetButtonUp("e"))
+        {
+            animate.SetBool("poured", false);
+            isPoured = false;
+        }
+    }
+}
