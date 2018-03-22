d38fb2477104b27782cc1b60882bad4c33c2f7c6
Valter-Wulfric-Sherbune-Jr <alienarereal@yahoo.com>
2018-03-21 23:27:56 -0400

    Evan Minimap Manually Merge

--- Chem Xplosion/Assets/Scripts/PuzzleOneTable.cs
+++ Chem Xplosion/Assets/Scripts/PuzzleOneTable.cs
@@ -2,47 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleOneTable : MonoBehaviour {
+public class PuzzleOneTable : MonoBehaviour
+{
 
-	public static bool isComplete_Puzzle_1 = false;
-	public static int puzzleOneCounter = 0;
-	public GameObject[] puzzleOneItems;
+    public static bool isComplete_Puzzle_1 = false;
+    public static int puzzleOneCounter = 0;
+    public static bool onTable = false;
+    public GameObject[] puzzleOneItems;
 
-	void Start () {
-		
-	}
-	void OnCollisionEnter(Collision col)
-	{
-		if (col.gameObject.tag == "Accept") {
-			puzzleOneCounter++;
-		}
-		if (col.gameObject.tag == "Deny") {
-			puzzleOneCounter--;
-		}
-		if (col.gameObject.tag == "Test"){
-			puzzleOneCounter++;
-		}
-	}
 
-	void OnCollisionExit(Collision col)
-	{
-		if (col.gameObject.tag == "Accept") {
-			puzzleOneCounter--;
-		}
-		if (col.gameObject.tag == "Deny") {
-			puzzleOneCounter++;
-		}
+    void Start()
+    {
 
-	}
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        onTable = true;
+        if (col.gameObject.tag == "Accept")
+        {
+            puzzleOneCounter++;
+        }
+        if (col.gameObject.tag == "Deny")
+        {
+            puzzleOneCounter--;
+        }
+        if (col.gameObject.tag == "Test")
+        {
+            puzzleOneCounter++;
+        }
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("e") && puzzleOneCounter == 3) {
-			isComplete_Puzzle_1 = true;
-			foreach(GameObject item in puzzleOneItems)
-			{
-				Destroy(item);
-			}
-		}
-	}
+    void OnCollisionExit(Collision col)
+    {
+        onTable = false;
+        if (col.gameObject.tag == "Accept")
+        {
+            puzzleOneCounter--;
+        }
+        if (col.gameObject.tag == "Deny")
+        {
+            puzzleOneCounter++;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("e") && puzzleOneCounter == 1)
+        {
+            isComplete_Puzzle_1 = true;
+            foreach (GameObject item in puzzleOneItems)
+            {
+                Destroy(item);
+            }
+        }
+    }
 }
