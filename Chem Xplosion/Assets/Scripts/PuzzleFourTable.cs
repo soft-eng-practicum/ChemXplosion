ae5dc24204b4fa7091e4b6ca4b3f29e61dee1806
Valter-Wulfric-Sherbune-Jr <alienarereal@yahoo.com>
2018-03-21 23:54:28 -0400

    Evan Minimap Manually Merge v2

---
+++ Chem Xplosion/Assets/Scripts/PuzzleFourTable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleFourTable : MonoBehaviour
+{
+
+    public static bool isComplete_Puzzle_4 = false;
+    public static int puzzleFourCounter = 0;
+    public GameObject[] puzzleFourItems;
+
+    void Start()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Accept")
+        {
+            puzzleFourCounter++;
+        }
+        if (col.gameObject.tag == "Deny")
+        {
+            puzzleFourCounter--;
+        }
+        if (col.gameObject.tag == "Test")
+        {
+            puzzleFourCounter++;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Accept")
+        {
+            puzzleFourCounter--;
+        }
+        if (col.gameObject.tag == "Deny")
+        {
+            puzzleFourCounter++;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("e") && puzzleFourCounter == 1)
+        {
+            isComplete_Puzzle_4 = true;
+            foreach (GameObject item in puzzleFourItems)
+            {
+                Destroy(item);
+            }
+        }
+    }
+}
