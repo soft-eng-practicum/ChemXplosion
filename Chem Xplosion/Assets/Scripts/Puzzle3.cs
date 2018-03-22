fa6566040eec1a37acb9df40c466d211344677ce
Rae <Rae.vroman@gmail.com>
2018-03-22 16:24:50 -0400

    Puzzle3 mostly completed, doors still need work

---
+++ Chem Xplosion/Assets/Scripts/Puzzle3.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3 : MonoBehaviour {
+
+    public static bool isComplete_Puzzle_3 = false;
+    public GameObject[] puzzle3Objects;
+    public Pour[] pours;
+    public GameObject explosion;
+
+    private void Start()
+    {
+        pours = new Pour[puzzle3Objects.Length];
+        for (int i = 0; i < puzzle3Objects.Length; i++)
+        {
+            pours[i] = puzzle3Objects[i].GetComponent<Pour>();
+        }
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Accept")
+        {
+            for (int i = 0; i < puzzle3Objects.Length; i++)
+            {
+                if (pours[i].isPoured)
+                {
+                    isComplete_Puzzle_3 = true;
+                }
+            }
+        }
+        else if (other.tag == "Deny")
+            for (int i = 0; i < puzzle3Objects.Length; i++)
+            {
+                if (pours[i].isPoured)
+                {
+                    isComplete_Puzzle_3 = false;
+                    Invoke("Explosion", 4);
+                }
+            }
+    }
+
+    public void Explosion()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
---
+++ Chem Xplosion/Assets/Scripts/RotateGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGenerator : MonoBehaviour {
+    public static bool puzzle3Complete; 
+
+    public GameObject gen1;
+    public GameObject gen2;
+    public GameObject gen3;
+    public GameObject gen4;
+
+    private bool playAudio;
+    AudioSource audiosrc;
+    public AudioClip generatorNoise;
+
+    // Use this for initialization
+    void Start () {
+        audiosrc = GetComponent<AudioSource>();
+        playAudio = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        puzzle3Complete = Puzzle3.isComplete_Puzzle_3;
+        if (puzzle3Complete)
+        {
+            gen1.transform.Rotate(0, 1, 0);
+            gen2.transform.Rotate(0, -1, 0);
+            gen3.transform.Rotate(0, 1, 0);
+            gen4.transform.Rotate(0, -1, 0);
+
+            if (!playAudio)
+            {
+                audiosrc.PlayOneShot(generatorNoise);
+                playAudio = true;
+            }
+        }
+
+
+
+    }
+}
