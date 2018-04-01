42d0bc731ed029b0dbd6fffb7058a113b7e58324
Valter-Wulfric-Sherbune-Jr <alienarereal@yahoo.com>
2018-04-01 03:52:23 -0400

    Manually Merge Development Test

---
+++ Chem Xplosion/Assets/Art/Minimap/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    public Transform player;
+
+    private void LateUpdate()
+    {
+
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+}
---
+++ Chem Xplosion/Assets/Scripts/MaterialChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour {
+    public Material[] mats;
+
+    public static int counter;
+    Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.sharedMaterial = mats[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (PuzzleOneTable.puzzleOneCounter == 0 && PuzzleOneTable.onTable == false)
+        {
+            rend.sharedMaterial = mats[0];
+        }
+        else if (PuzzleOneTable.puzzleOneCounter == 1)
+        {
+            rend.sharedMaterial = mats[1];
+        }
+        else
+        {
+            rend.sharedMaterial = mats[2];
+        }
+
+    }
+}
+
