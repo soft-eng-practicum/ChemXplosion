473aeaee0a990017a501768ff2af3521ec4d85e4
Rae <Rae.vroman@gmail.com>
2018-04-22 23:04:40 -0400

    finishing touches, runthrough to make sure puzzles work

---
+++ Chem Xplosion/Assets/Scripts/HideUI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class HideUI : MonoBehaviour {
+
+    public string buttonName;
+    bool visible;
+    CanvasGroup canvasGroup;
+    public CharacterController characterController;
+
+    // Use this for initialization
+    void Start()
+    {
+        visible = false;
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (Input.GetButtonUp(buttonName))
+        {
+            if(visible)
+            {
+                Hide();
+            }
+            else if(!visible)
+            {
+                Show();
+            }
+
+
+        }
+
+    }
+
+    void Hide()
+    {
+        canvasGroup.alpha = 0f; //this makes everything transparent
+        canvasGroup.blocksRaycasts = false; //this prevents the UI element to receive input events
+        visible = false;
+        canvasGroup.interactable = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+        characterController.enabled = true;
+        characterController.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    void Show()
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        visible = true;
+        canvasGroup.interactable = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        characterController.enabled = false;
+        characterController.GetComponent<FirstPersonController>().enabled = false;
+    }
+}
---
+++ Chem Xplosion/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject ps;
+    Text winGameText;
+
+    private void Start()
+    {
+        winGameText = GameObject.Find("WinText").GetComponent<Text>();
+      // ps = GetComponentInChildren<ParticleSystem>();
+    }
+    void Update()
+    {
+        if (PuzzleOneTable.isComplete_Puzzle_1 &&
+            Puzzle2.isComplete_Puzzle_2 &&
+            Puzzle3.isComplete_Puzzle_3 &&
+            PuzzleFourTable.isComplete_Puzzle_4)
+        {//If the four puzzles have been completed, activate the teleporter.
+            ps.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (PuzzleOneTable.isComplete_Puzzle_1 &&
+            Puzzle2.isComplete_Puzzle_2 &&
+            Puzzle3.isComplete_Puzzle_3 &&
+            PuzzleFourTable.isComplete_Puzzle_4)
+            {
+                winGameText.text = "Congratulations, you have completed level 1";//Alter this to begin level 2 scene;
+                StartCoroutine("winText");
+                winGameText.text = "";
+                print("winning");
+            }
+                
+        }
+    }
+
+    private IEnumerator winText() {
+        yield return new WaitForSeconds(2);
+    }
+}
